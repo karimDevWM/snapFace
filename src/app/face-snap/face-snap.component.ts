8b100a521d996244b9c9fdc81fffd77c71966d35
devjava69 <k.trabelsi9@laposte.net>
2024-07-25 08:54:51 +0200

    refactor methods

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,39 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
+
   snapped!: boolean;
   snapButtonText!: string;
 
   ngOnInit(): void {
-    this.title = 'Archibald et ses copains'
-    this.description = 'Mon meilleur ami depuis toujours !';
-    this.createdAt = new Date();
-    this.snaps = 0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2024/03/01/12/46/watercolor-teddy-bear-8606504_1280.png';
     this.snapped = false;
     this.snapButtonText = "Oh Snap!";
   }
 
   onSnap(): void {
-    if(this.snapped==false){
-      this.addSnap();
-    } else if(this.snapped==true && this.snaps > 0) {
+    if(this.snapped){
       this.unSnap();
+    } else {
+      this.snap();
     }
   }
 
   unSnap() {
-    this.snaps--;
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap!';
+    this.snapped = false;
   }
 
-  addSnap() {
-    this.snaps++;
-    this.snapped=true;
-    this.snapButtonText="Oups !, unsnapped";
+  snap(){
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Oops, unSnap!';
+    this.snapped = true;
   }
 }
